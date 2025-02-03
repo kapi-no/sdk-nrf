e93431d00b47147b07d7dac18966cd25b5ff3d0c
Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>
2025-02-03 13:55:18 +0100

    applications: nrf_desktop: lower frequency in USB suspend on nRF54H
    Added a feature to the USB State PM module that is used to lower system
    frequency in the USB suspended state to limit the power consumption.
    The feature is only available for the nRF54H series that supports the
    nrfs software and its GDFS service.
    
    Ref: NCSDK-31375
    
    Signed-off-by: Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>

--- applications/nrf_desktop/src/modules/usb_state_pm.c
+++ applications/nrf_desktop/src/modules/usb_state_pm.c
@@ -3,6 +3,7 @@
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
+#include <zephyr/drivers/clock_control/nrf_clock_control.h>
 #include <zephyr/kernel.h>
 #include <zephyr/types.h>
 
@@ -17,6 +18,39 @@ LOG_MODULE_REGISTER(MODULE, CONFIG_DESKTOP_USB_STATE_LOG_LEVEL);
 #include <caf/events/power_manager_event.h>
 #include <caf/events/force_power_down_event.h>
 
+static int global_domain_frequency_set(void)
+{
+	int err;
+	int res;
+	struct onoff_client cli;
+	const struct nrf_clock_spec clk_spec_global_hsfll = {
+		.frequency = MHZ(64)
+	};
+	const struct device *hsfll_dev = DEVICE_DT_GET(DT_NODELABEL(hsfll120));
+
+	if (!IS_ENABLED(CONFIG_DESKTOP_USB_PM_LOWER_FREQ_ON_SUSPENDED)) {
+		return 0;
+	}
+
+	LOG_INF("Requested frequency [Hz]: %d", clk_spec_global_hsfll.frequency);
+	sys_notify_init_spinwait(&cli.notify);
+	err = nrf_clock_control_request(hsfll_dev, &clk_spec_global_hsfll, &cli);
+	LOG_INF("Return code: %d", err);
+	__ASSERT_NO_MSG(err < 3);
+	__ASSERT_NO_MSG(err >= 0);
+	do {
+		err = sys_notify_fetch_result(&cli.notify, &res);
+		k_yield();
+	} while (err == -EAGAIN);
+	LOG_INF("Clock control request return value: %d", err);
+	LOG_INF("Clock control request response code: %d", res);
+	__ASSERT_NO_MSG(err == 0);
+	__ASSERT_NO_MSG(res == 0);
+
+	return 0;
+}
+
+SYS_INIT(global_domain_frequency_set, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
 
 static bool app_event_handler(const struct app_event_header *aeh)
 {
